3becdc9f6335272d41f8120595c6e751b30645d8
“Saiyedul <saiyedul.abrar@gmail.com>
2025-08-13 23:09:19 +0600

    database

--- library_project/settings.py
+++ library_project/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'library_project.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('dbname'),
+        'USER': config('user'),
+        'PASSWORD': config('password'),
+        'HOST': config('host'),
+        'PORT': config('port')
     }
 }
 
